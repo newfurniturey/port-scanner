970a3c136701e601897431afc49b29db6155894a
Joey <newfurniturey@gmail.com>
2015-03-21 00:53:39 -0400

    Single-threaded port enumeration

--- src/com/newfurniturey/portscanner/PortScanner.java
+++ src/com/newfurniturey/portscanner/PortScanner.java
@@ -1,9 +1,27 @@
 package com.newfurniturey.portscanner;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 class PortScanner {
     
     public static void main(String[] args) {
+        final int timeout = 200;
+        final String host = args[0];
         
+        for (int port = 1; port <= 65535; port++) {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(host, port), timeout);
+                socket.close();
+                
+                // if we made it here, the port is open!
+                System.out.printf("Port %d is open%n", port);
+            } catch (Exception e) {
+                // safely ignore... port is closed
+            }
+        }
     }
     
 }
+
